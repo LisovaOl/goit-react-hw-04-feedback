73bbbbabf1100c60cc8bd8aa08e59b6236f6c42d
LisovaOl <lisova.olenka@gmail.com>
2022-10-17 06:38:26 +0300

    Hello

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,8 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React FeedBack
+    <div>
+        React FeedBack
       Hello Olena
-
     </div>
   );
 };
