e71a881b33cae3b6a5cdc41c63208156602d4e16
LisovaOl <lisova.olenka@gmail.com>
2022-10-17 07:17:27 +0300

    YPA!!!!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 
-import Form from './Form'
 
 export const App = () => {
   return (
     <div>
       React FeedBack Hello Olena
-      <Form />
-      <ul>
-        <li>React FeedBack Hello Olena</li>
-        <li>React FeedBack Hello Olena</li>
-        <li>React FeedBack Hello Olena</li>
-        <li>React FeedBack Hello Olena</li>
-        <li>React FeedBack Hello Olena</li>
-      </ul>
     </div>
   );
 };
